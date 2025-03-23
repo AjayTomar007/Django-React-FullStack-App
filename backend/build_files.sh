df2018c76aa63b2e40ac238bbb32bfc9cff6828e
Ajay <ajtomar1234@gmail.com>
2025-03-23 23:32:27 +0530

    modified build fiel and settings

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,4 +2,5 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
+mv staticfiles staticfiles_build  # Rename to what Vercel expects
 echo "BUILD END"
