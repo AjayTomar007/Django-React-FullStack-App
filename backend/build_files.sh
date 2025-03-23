97609b0fc6c5704d89ff566e28d7959bf218eb06
Ajay <ajtomar1234@gmail.com>
2025-03-23 22:59:04 +0530

    modified build file again

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,5 @@
-
+#!/bin/bash
 echo "BUILD START"
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
 echo "BUILD END"
