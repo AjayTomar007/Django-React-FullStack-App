7be1a3e8f39e572731b758dd4ba7c273199af1b6
Ajay <ajtomar1234@gmail.com>
2025-03-24 00:02:09 +0530

    updated

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,9 +2,11 @@
 echo "BUILD START"
 mkdir -p staticfiles_build/static
 python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
+python3.10 -m pip install -r backend/requirements.txt
 
-# Collect static files
+# Run migrations and collect static files
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
 python3.10 manage.py collectstatic --no-input --clear
 echo "BUILD END"
 ls -la staticfiles_build  # Debug: list the directory contents
