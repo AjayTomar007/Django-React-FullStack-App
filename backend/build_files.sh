ededab708469b5570482afe019151e5fc3f062e7
Ajay <ajtomar1234@gmail.com>
2025-03-23 23:52:24 +0530

    python version fixed in build

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "BUILD START"
+mkdir -p staticfiles_build/static
 python3.10 -m pip install -r requirements.txt
 python3.10 manage.py collectstatic --no-input --clear
 echo "BUILD END"
+ls -la staticfiles_build  # Debug: list the directory contents
